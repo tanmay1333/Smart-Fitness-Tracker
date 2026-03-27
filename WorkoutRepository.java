8d0dac4ecd137f11b3580f791740f934bdd7a4d5
tanmay1333 <tanmaysingha2006@gmail.com>
2026-03-27 23:54:10 +0530

    Implement WorkoutRepository for saving and loading workouts
    Added WorkoutRepository for data persistence

---
+++ WorkoutRepository.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+class WorkoutRepository {
+
+    static void saveWorkouts(List<Workout> workouts) {
+        try {
+            FileWriter fw = new FileWriter("workouts.txt");
+            for (Workout w : workouts) {
+                fw.write(w.name + "," + w.duration + "\n");
+            }
+            fw.close();
+        } catch (Exception e) {
+            System.out.println("Error saving data");
+        }
+    }
+
+    static List<Workout> loadWorkouts() {
+        List<Workout> list = new ArrayList<>();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("workouts.txt"));
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+                list.add(new CardioWorkout(data[0], Integer.parseInt(data[1])));
+            }
+            br.close();
+        } catch (Exception e) {
+            System.out.println("No previous data found");
+        }
+        return list;
+    }
+}
